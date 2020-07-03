87a6f269565d0be0dfb60b792893e5f0927acb0a
Cate <ceh2181@columbia.edu>
2020-07-03 11:54:20 -0400

    WIP: Define findUserByEmail
    - Still experiencing problems with asynchronous calls

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,29 @@ const router = express.Router();
 
 const validator = require('validator');
 const bcrypt = require('bcrypt');
-
 const passport = require('passport');
+
+const db = require('../db');
+
+/**
+ * Find user by email in database.
+ *
+ * @param {String}  email   the given email.
+ * @return {Int}   userId  userId if user exists or NULL if no user exists.
+ */
+const findUserByEmail = (email) => {
+  db.query('SELECT user_id FROM users WHERE email=$1', [email])
+    .then((results) => {
+      //e.g. no results found
+      if (results.rowCount == 0) return null;
+
+      const userID = results.rows[0].user_id;
+
+      console.log('userID', userID);
+      return userID;
+    })
+    .catch((e) => console.error(e));
+};
 
 //all routes prepended by auth
 router.post('/signup', (req, res) => {
@@ -17,7 +38,18 @@ router.post('/signup', (req, res) => {
 
   //proceed to signup
   else {
-    res.status(201).json(email);
+    const user = findUserByEmail(email);
+
+    console.log('user', user);
+
+    //check if email address is in use
+    if (user != null) {
+      res.status(400).send('A user is already registered with this address');
+    }
+    //create new user
+    else {
+      res.status(201).send('sign up');
+    }
   }
 });
 
