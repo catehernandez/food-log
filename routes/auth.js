db0dbe6dbc68ddaa0812d3290e22aad4b7330459
Cate <ceh2181@columbia.edu>
2020-07-06 22:45:05 -0400

    Install express-promise-router
    - Handle async exceptions without needing to define a rejection handler

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express-promise-router')();
 
 const validator = require('validator');
 const bcrypt = require('bcrypt');
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express-promise-router')();
 
 const db = require('../db');
 
