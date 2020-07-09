1908b008a84d53b1f8be969aa75460517f12970c
Cate <ceh2181@columbia.edu>
2020-07-08 22:15:34 -0400

    WIP: Build routes for creating and retrieving logs

---
+++ server/db/logs.js
@@ -0,0 +1,61 @@
+const db = require('./index');
+
+/**
+ * Retrieves a user's log for the given date. Returns null if no log is defined.
+ *
+ * @param { Integer } user_id
+ * @param { Date } date
+ */
+const findLog = (user_id, date) => {
+  return db
+    .query('SELECT * FROM logs WHERE user_id = $1 AND log_date=$2', [
+      user_id,
+      date,
+    ])
+    .then((results) => {
+      if (results.rowCount == 0) return null;
+
+      return results.rows[0];
+    })
+    .catch((e) => console.error(e));
+};
+
+/**
+ * Creates a new log for the given date and returns the inserted entry.
+ *
+ * @param {Integer} user_id
+ * @param {Date} date   Date for the new entry.
+ */
+const createLog = (user_id, date) => {
+  return db
+    .query('INSERT INTO logs (user_id, log_date) VALUES ($1, $2) RETURNING *', [
+      user_id,
+      date,
+    ])
+    .then((results) => {
+      return results.rows;
+    })
+    .catch((err) => console.error(err));
+  /** Handle error if trying to insert duplicate */
+  /* returns err code 23505 */
+};
+
+/**
+const updateLog = async (user_id, date) */
+
+/**
+ * Returns an array of all a user's past logs in JSON format. Returns an empty
+ * array if none are found.
+ *
+ * @param {Integer} user_id
+ */
+const getAllUserLogs = (user_id) => {
+  return db
+    .query('SELECT * FROM logs WHERE user_id=$1', [user_id])
+    .then((results) => {
+      return results.rows;
+    })
+    .catch((e) => console.error(error));
+};
+
+module.exports = { findLog, createLog, getAllUserLogs };
--- server/routes/dash.js
+++ server/routes/dash.js
@@ -4,8 +4,31 @@
 const router = require('express-promise-router')();
 const { ensureLoggedIn } = require('connect-ensure-login');
 
+const LogsDB = require('../db/logs');
+
+/** testing */
 router.get('/dashboard', ensureLoggedIn('auth/login'), (req, res) => {
   res.status(200).json({ user: req.user });
 });
 
+router.post('/createLog', ensureLoggedIn('auth/login'), async (req, res) => {
+  const now = new Date();
+  const results = await LogsDB.createLog(req.user.user_id, now);
+
+  res.status(200).send(results);
+});
+
+router.get('/allLogs', ensureLoggedIn('/auth/login'), async (req, res) => {
+  const results = await LogsDB.getAllUserLogs(req.user.user_id);
+
+  res.status(200).send(results);
+});
+
+router.get('/currentLog', ensureLoggedIn('/auth/login'), async (req, res) => {
+  const now = new Date();
+
+  const results = await LogsDB.findLog(req.user.user_id, now);
+
+  res.status(200).send(results);
+});
 module.exports = router;
