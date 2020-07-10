0f13d5c12acdea82cc54d4b2e21038a84b3b0dd9
Cate <ceh2181@columbia.edu>
2020-07-09 21:52:50 -0400

    Configure routes to 2 main pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  return <div className="App">Hello, world!</div>;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/login" exact component={Login} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
