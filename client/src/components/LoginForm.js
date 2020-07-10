fd5df1df530425f6f5a850fbd5bfd844fd214d75
Cate <ceh2181@columbia.edu>
2020-07-10 16:07:19 -0400

    WIP: Add validation schema with Yup

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import { Form, Input, Label } from 'components/Form';
 import Button from 'components/Button';
@@ -7,6 +8,9 @@ import Button from 'components/Button';
 const LoginForm = () => {
   const formik = useFormik({
     initialValues: { email: '', password: '' },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Required'),
+    }),
     onSubmit: (values) => {
       console.log(values);
     },
@@ -19,15 +23,12 @@ const LoginForm = () => {
         name="email"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
       />
-      <Label htmlFor="password">Password</Label>
-      <Input
-        name="password"
-        type="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
       <Button>Log in</Button>
     </Form>
   );
