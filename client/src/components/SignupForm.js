bee5bb33b9395adf332c3332f4aa54fa842ba015
Cate <ceh2181@columbia.edu>
2020-07-11 11:20:58 -0400

    WIP: Create signup form

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import {
+  StyledInput,
+  StyledLabel,
+  StyledErrorMessage,
+} from 'components/StyledInput';
+import Button from 'components/Button';
+
+const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SignupForm = () => {
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={Yup.object({
+        email: Yup.string().required('Required').email('Please enter an email'),
+        password: Yup.string()
+          .required('Required')
+          .min(8, 'Password must be at least 8 characters')
+          .max(35, 'Password must be less than 35 characters'),
+      })}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <StyledForm>
+          <StyledLabel htmlFor="email">Email</StyledLabel>
+          <Field as={StyledInput} name="email" />
+          <ErrorMessage component={StyledErrorMessage} name="email" />
+          <StyledLabel htmlFor="password">Password</StyledLabel>
+          <Field as={StyledInput} name="password" type="password" />
+          <ErrorMessage component={StyledErrorMessage} name="password" />
+          <Button
+            disabled={
+              !touched.email ||
+              !touched.password ||
+              errors.email ||
+              errors.password
+            }
+          >
+            Sign up
+          </Button>
+        </StyledForm>
+      )}
+    </Formik>
+  );
+};
+
+export default SignupForm;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import SignupForm from 'components/SignupForm';
+
+const SignupContainer = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 30%;
+  transform: translate(-50%, 30%);
+  width: 30vw;
+`;
 
 const Signup = () => {
   return (
-    <div>
-      <div>Sign up</div>
+    <SignupContainer>
+      <SignupForm />
       <div>
         Already have an account? <Link to="/login">Log in</Link>
       </div>
-    </div>
+    </SignupContainer>
   );
 };
 
