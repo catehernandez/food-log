8d0f2ac93c4b8cbb8eae3edc8d923834dd8f4451
Cate <ceh2181@columbia.edu>
2020-07-11 16:43:29 -0400

    WIP: Configure redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
+//dummy reducer for init setup only
+const store = createStore(() => [], {}, applyMiddleware());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
