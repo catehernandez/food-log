2144f089f54e16d1cda1a97210d5083e50ceaf6c
Cate <ceh2181@columbia.edu>
2020-07-12 23:10:12 -0400

    WIP: Define auth actions & auth reducers

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,20 @@
-export default function reducer(state = {}, action) {
+import * as actions from 'actions/authActions';
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  hasErrors: false,
+};
+
+export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case actions.GET_CURRENT_USER:
+      return { ...state, loading: true };
+    case actions.GET_CURRENT_USER_SUCCESS:
+      return { currentUser: action.payload, loading: false, hasErrors: false };
+    case actions.GET_CURRENT_USER_FAILURE:
+      return { ...state, loading: false, hasErrors: true };
+
     default:
       return state;
   }
