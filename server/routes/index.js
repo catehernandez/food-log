823b5fcba8ea2874777d45291f5450b6bc1bd865
Cate <ceh2181@columbia.edu>
2020-07-18 10:32:30 -0400

    WIP: Move /logs to under /users
    - logs are always tied to users; makes most sense as a subcollection
    - Still working on reorganizing some routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,8 @@
 /** Routes each individual router into main app */
 const users = require('./users');
 const auth = require('./auth');
-const log = require('./log');
 
 module.exports = (app) => {
   app.use('/users', users);
   app.use('/auth', auth);
-  app.use('/log', log);
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,9 @@
 const router = require('express-promise-router')();
 
 const db = require('../db');
+const LogsDB = require('../db/logs');
 
-/* testing */
+/* getUsers route for testing only*/
 const getUsers = (req, res) => {
   db.query('SELECT * FROM users')
     .then((results) => {
@@ -21,8 +22,49 @@ const getCurrentUser = async (req, res) => {
   }
 };
 
+const getUsersLogs = async (req, res) => {
+  if (!req.user) {
+    res.status(401).json('Unauthorized');
+  } else {
+    const user = req.user;
+    const results = await LogsDB.getAllUserLogs(user.user_id);
+
+    res.status(200).json(results);
+  }
+};
+
+//get date from body or params?
+const createLog = async (req, res) => {
+  if (!req.user) return res.status(401).json('Unauthorized');
+
+  const user = req.user;
+  const { date } = req.body;
+
+  try {
+    const newLog = await LogsDB.createLog(user.user_id, date);
+    res.status(200).json(newLog);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+router.get('/currentLog', async (req, res) => {
+  if (!req.user) return res.status(401).json('Unauthorized');
+
+  //for testing only -- determine time on client end, i think
+  //so that "now" is dependent on user & not server timezone
+  const now = new Date();
+
+  const user = req.user;
+  const results = await LogsDB.findLog(user.user_id, now);
+
+  res.status(200).json(results);
+});
+
 //all routes prepended by /users
 router.get('/', getUsers);
 router.get('/current', getCurrentUser);
+router.get('/current/logs', getUsersLogs);
+router.post('/current/logs', createLog);
 
 module.exports = router;
