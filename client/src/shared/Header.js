c79879acb0c277afddb654088f20d523e47f34ec
Cate <ceh2181@columbia.edu>
2020-07-18 14:36:03 -0400

    Position header

--- client/src/shared/Header.js
+++ client/src/shared/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import LogoutButton from 'session/LogoutButton';
 
+const HeaderContainer = styled.header`
+  float: right;
+  padding: 1.5rem;
+`;
+
 const Header = () => {
-  return <LogoutButton />;
+  return (
+    <HeaderContainer>
+      <LogoutButton />
+    </HeaderContainer>
+  );
 };
 
 export default Header;
