2d4cac350b8c298057975e37bed8b94d6dc3ab7d
Cate <ceh2181@columbia.edu>
2020-07-21 11:32:47 -0400

    Cleanup consolelogs & streamline user route res status on err

--- server/user/userHandlers.js
+++ server/user/userHandlers.js
@@ -10,7 +10,7 @@ module.exports = {
     }
   },
   getAllLogs: async (req, res) => {
-    if (!req.user) return res.status(401).json('Unauthorized');
+    if (!req.user) return res.sendStatus(401);
 
     //user is authenticated
     try {
@@ -18,7 +18,7 @@ module.exports = {
       const logs = await LogsDB.getAllUserLogs(user.user_id);
 
       //e.g. no logs found
-      if (logs.length === 0) return res.status(404).json(null);
+      if (logs.length === 0) return res.sendStatus(404);
 
       res.status(200).json(logs);
     } catch {
@@ -26,7 +26,7 @@ module.exports = {
     }
   },
   createLog: async (req, res) => {
-    if (!req.user) return res.status(401).json('Unauthorized');
+    if (!req.user) return res.sendStatus(401);
 
     //user is authenticated
     try {
@@ -40,18 +40,18 @@ module.exports = {
     }
   },
   getLog: async (req, res) => {
-    if (!req.user) return res.status(401).json('Unauthorized');
+    if (!req.user) return res.sendStatus(401);
 
     //user is authenticated
     try {
       const { user_id } = req.user;
       const { date } = req.params;
 
-      console.log(date);
+      //console.log(date);
       const log = await LogsDB.findLog(user_id, date);
 
       //if not log is found
-      if (!log) return res.status(404).json(null);
+      if (!log) return res.sendStatus(404);
 
       res.status(200).json(log);
     } catch (err) {
