051b3521b30a8cd5ea8ba5c6eb29c0d43ed933e6
Cate <ceh2181@columbia.edu>
2020-07-20 20:16:46 -0400

    Use redux-devtools-extension

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import axios from 'axios';
 
 import App from './App';
@@ -16,7 +17,10 @@ axios.defaults.baseURL = 'http://localhost:5000';
 axios.defaults.headers.post['Content-Type'] =
   'application/x-www-form-urlencoded';
 //dummy reducer for init setup only
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
