5a87804a748c958ca3b871f794d568a38a222422
Cate <ceh2181@columbia.edu>
2020-07-22 22:22:10 -0400

    WIP: Create visual representation of current log
    - Represent with checkboxes. Only the most recently checked checkbox and
      the first unchecked checkbox should be checkable (to preserve visual
      coherence of serving--checkboxes should be checked in order)
    - WIP: Develop handleChange method

--- client/src/logs/Servings.js
+++ client/src/logs/Servings.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Checkbox from './Checkbox';
+
 const ServingsContainer = styled.div``;
 
+//TODO: This will become "updateCount"
+const handleChange = (event) => {
+  console.log(event.target.checked);
+};
+
 const Servings = (props) => {
-  return (
-    <ServingsContainer>
-      <div>Goals: {props.goals}</div>
-      <div>Completed: {props.completed}</div>
-    </ServingsContainer>
-  );
+  const boxes = [];
+  for (let i = 0; i < props.goals; i++) {
+    let key = i.toString();
+
+    //default checked, enabled
+    if (i < props.completed - 1) {
+      boxes.push(<Checkbox key={key} checked={true} readOnly={true} />);
+    }
+    //default checked; enabled
+    else if (i === props.completed - 1) {
+      boxes.push(
+        <Checkbox key={key} defaultChecked={true} onChange={handleChange} />
+      );
+    }
+    //default unchecked; enabled
+    else if (i === props.completed) {
+      boxes.push(
+        <Checkbox key={key} defaultChecked={false} onChange={handleChange} />
+      );
+    }
+    //else unchecked & disabled
+    else {
+      boxes.push(<Checkbox key={key} checked={false} readOnly={true} />);
+    }
+  }
+
+  return <ServingsContainer>{boxes}</ServingsContainer>;
 };
 
 export default Servings;
