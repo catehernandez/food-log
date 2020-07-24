e0b00309a2972ad8a751f05038955c21cc04a333
Cate <ceh2181@columbia.edu>
2020-07-23 21:59:53 -0400

    Validate updateLog requests before querying db

--- server/user/userRoutes.js
+++ server/user/userRoutes.js
@@ -6,5 +6,6 @@ router.get('/', userHandlers.getCurrentUser);
 router.get('/logs', userHandlers.getAllLogs);
 router.post('/logs', userHandlers.createLog);
 router.get('/logs/:date', userHandlers.getLog);
+router.patch('/logs/:date', userHandlers.updateLog);
 
 module.exports = router;
