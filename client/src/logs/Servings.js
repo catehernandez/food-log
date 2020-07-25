2f87dc01bf714ea822fda2e1415b2ee2b8947c7f
Cate <ceh2181@columbia.edu>
2020-07-25 00:19:08 -0400

    WIP: Convert servings to class-based component

--- client/src/logs/Servings.js
+++ client/src/logs/Servings.js
@@ -5,39 +5,48 @@ import Checkbox from './Checkbox';
 
 const ServingsContainer = styled.div``;
 
-//TODO: This will become "updateCount"
-const handleChange = (event) => {
-  console.log(event.target.checked);
-};
+class Servings extends React.Component {
+  handleChange = (event) => {
+    console.log(event.target.checked);
+  };
 
-const Servings = (props) => {
-  const boxes = [];
-  for (let i = 0; i < props.goals; i++) {
-    let key = i.toString();
+  render() {
+    const boxes = [];
+    for (let i = 0; i < this.props.goals; i++) {
+      let key = i.toString();
 
-    //default checked, enabled
-    if (i < props.completed - 1) {
-      boxes.push(<Checkbox key={key} checked={true} readOnly={true} />);
+      //default checked, enabled
+      if (i < this.props.completed - 1) {
+        boxes.push(<Checkbox key={key} checked={true} readOnly={true} />);
+      }
+      //default checked; enabled
+      else if (i === this.props.completed - 1) {
+        boxes.push(
+          <Checkbox
+            key={key}
+            defaultChecked={true}
+            onChange={this.handleChange}
+          />
+        );
+      }
+      //default unchecked; enabled
+      else if (i === this.props.completed) {
+        boxes.push(
+          <Checkbox
+            key={key}
+            defaultChecked={false}
+            onChange={this.handleChange}
+          />
+        );
+      }
+      //else unchecked & disabled
+      else {
+        boxes.push(<Checkbox key={key} checked={false} readOnly={true} />);
+      }
     }
-    //default checked; enabled
-    else if (i === props.completed - 1) {
-      boxes.push(
-        <Checkbox key={key} defaultChecked={true} onChange={handleChange} />
-      );
-    }
-    //default unchecked; enabled
-    else if (i === props.completed) {
-      boxes.push(
-        <Checkbox key={key} defaultChecked={false} onChange={handleChange} />
-      );
-    }
-    //else unchecked & disabled
-    else {
-      boxes.push(<Checkbox key={key} checked={false} readOnly={true} />);
-    }
+
+    return <ServingsContainer>{boxes}</ServingsContainer>;
   }
-
-  return <ServingsContainer>{boxes}</ServingsContainer>;
-};
+}
 
 export default Servings;
