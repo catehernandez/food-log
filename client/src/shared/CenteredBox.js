3f4ffbc3d24e5655aa5e5a2b218bbc492e536859
Cate <ceh2181@columbia.edu>
2020-07-29 00:28:24 -0400

    Reposition centered box higher on page

--- client/src/shared/CenteredBox.js
+++ client/src/shared/CenteredBox.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const CenteredBox = styled.div`
   background-color: #efefef;
   border-radius: 12px;
+  max-height: 340px;
   left: 50%;
   position: absolute;
-  top: 30%;
-  transform: translate(-50%, 30%);
+  top: 17%;
+  transform: translate(-50%, 17%);
   width: 340px;
 
   @media (min-width: ${(props) => props.theme.breakpoints.small}) {
