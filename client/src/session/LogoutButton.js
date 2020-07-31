d3bf43e9aca1286caf50ba84ac0b87f81397a03e
Cate <ceh2181@columbia.edu>
2020-07-31 10:32:34 -0400

    Style logout button

--- client/src/session/LogoutButton.js
+++ client/src/session/LogoutButton.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import * as sessionActions from 'session/sessionRedux';
+import Button from 'shared/Button';
 
 const LogoutButton = (props) => {
   const logout = () => {
     props.logout();
   };
 
-  return <button onClick={logout}>Logout</button>;
+  return <Button onClick={logout}>Logout</Button>;
 };
 
 export default connect(null, sessionActions)(LogoutButton);
--- client/src/shared/Button.js
+++ client/src/shared/Button.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: #efefef;
+  border: 1px solid ${({ theme }) => theme.colors.brownBlack};
+  border-radius: 4px;
+  cursor: pointer;
+  display: inline-block;
+  font: 1rem ${({ theme }) => theme.fontFamily.Quicksand};
+  letter-spacing: 0.3px;
+  padding: 0.5rem 1.5rem;
+  text-align: center;
+  text-decoration: none;
+`;
 
 export default Button;
