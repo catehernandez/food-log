6b2bf404e27844b33a64a347e8537c9ac5fa8d76
Cate <ceh2181@columbia.edu>
2020-08-01 17:55:39 -0400

    WIP: Style login form input field

--- client/src/shared/StyledInput.js
+++ client/src/shared/StyledInput.js
@@ -5,7 +5,20 @@ export const StyledLabel = styled.label`
 `;
 
 export const StyledInput = styled.input`
-  margin: 0.5rem 0;
+  background: none;
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.brownBlack};
+  font-family: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  margin: 1rem 0;
+  padding: 0.5rem;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.brownBlack};
+    border-radius: 4px;
+    outline: 0;
+  }
 `;
 
 export const StyledErrorMessage = styled.div`
