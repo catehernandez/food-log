739395b4f916bfd5e0c20352eb29b78fe16d5975
Cate <ceh2181@columbia.edu>
2020-08-01 11:39:10 -0400

    Add padding to user icon in header

--- client/src/shared/Header.js
+++ client/src/shared/Header.js
@@ -2,18 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 import LogoutButton from 'session/LogoutButton';
-import { ReactComponent as UserIcon } from 'userProfile/user-icon.svg';
+import { ReactComponent as UserIcon } from 'user/user-icon.svg';
 
 const HeaderContainer = styled.header`
   display: flex;
   justify-content: flex-end;
-  padding: 1.5rem;
 `;
+
+const UserIconContainer = styled.span`
+  cursor: pointer;
+  margin: 0 1.2rem;
+`;
+
+const showUserProfile = () => {
+  console.log('user profile');
+};
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <UserIcon />
+      <UserIconContainer onClick={showUserProfile}>
+        <UserIcon />
+      </UserIconContainer>
       <LogoutButton />
     </HeaderContainer>
   );
