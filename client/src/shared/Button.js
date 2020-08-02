31d341cb13cc1c86e17369101be3dc621d19fa85
Cate <ceh2181@columbia.edu>
2020-08-02 16:12:10 -0400

    Center Button contents

--- client/src/shared/Button.js
+++ client/src/shared/Button.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  align-items: center;
   background-color: #efefef;
   border: 1px solid ${({ theme }) => theme.colors.brownBlack};
   border-radius: 4px;
   cursor: pointer;
-  display: inline-block;
+  display: flex;
   font: 1rem ${({ theme }) => theme.fontFamily.Quicksand};
+  justify-content: center;
   letter-spacing: 0.3px;
   padding: 0.5rem 1.5rem;
   text-align: center;
