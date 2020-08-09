21094d6cae56fe0bd560bd4e33d51d642c8138d6
Cate <ceh2181@columbia.edu>
2020-08-09 19:03:40 -0400

    WIP: Configure redux-state-sync
    - Redux is syncing across tabs, but need Servings checkboxes to reflect
      changes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
+import { withReduxStateSync } from 'redux-state-sync';
 
 import session from 'session/sessionRedux';
 import log from 'logs/logRedux';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   session,
   log,
 });
+
+export default withReduxStateSync(rootReducer);
