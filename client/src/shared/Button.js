2f40c8fabad38ea3599e27eab699afc896c79255
Cate <ceh2181@columbia.edu>
2020-08-08 21:19:20 -0400

    Style disabled Button

--- client/src/shared/Button.js
+++ client/src/shared/Button.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   align-items: center;
@@ -15,6 +15,14 @@ const Button = styled.button`
   text-decoration: none;
   text-transform: capitalize;
   width: max-content;
+
+  ${(props) =>
+    props.disabled &&
+    css`
+      border-color: ${(props) => props.theme.colors.brownBlack60};
+      color: ${(props) => props.theme.colors.brownBlack60};
+      cursor: auto;
+    `}
 `;
 
 export default Button;
--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -4,6 +4,7 @@ const theme = {
     lightBeige: 'hsl(42,40%,91%)',
     lightestBeige: 'hsl(42,40%,95%)',
     brownBlack: 'hsl(0,21.3%,24.9%)',
+    brownBlack60: 'hsla(0,21.3%,24.9%, 0.6)',
     green: 'hsl(144.1,86%,60.8%)',
     pink: 'hsl(331.2,91.4%,63.3%)',
     lightBlue: 'hsl(197.9,91.4%,63.3%)',
