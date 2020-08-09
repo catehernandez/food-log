95f06bfc347135b676f02d348eba9b63b1198f92
Cate <ceh2181@columbia.edu>
2020-08-08 20:39:40 -0400

    Set app background color and font color on body, inputs, and buttons

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -4,11 +4,13 @@ const GlobalStyle = createGlobalStyle`
   html,
   body,
   #root {
+    background-color: ${({ theme }) => theme.colors.lightestBeige};
     font-size: 16px; 
     height: 100%;
     margin: 0;
   }
-  body{
+  body, input, button {
+    color: ${({ theme }) => theme.colors.brownBlack};
     font-family: ${({ theme }) => theme.fontFamily.Quicksand};
   }
 `;
