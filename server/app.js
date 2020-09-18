125285610234177c2b44c1f7dc6c5855c0b855a0
Cate <ceh2181@columbia.edu>
2020-09-18 13:58:20 -0400

    Configure server/app.js to serve react front-end
    - Modeled based on server/index.js in heroku-cra-node

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cluster = require('cluster');
 const cors = require('cors');
 const helmet = require('helmet');
+const numCPUs = require('os').cpus().length;
+const path = require('path');
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
+
+const pool = require('./db');
+const mountRoutes = require('./routes');
 
 //Authentication packages
 const session = require('express-session');
@@ -11,31 +17,17 @@ const pgSession = require('connect-pg-simple')(session);
 const passport = require('passport');
 require('./services/passport');
 
-const pool = require('./db');
-const mountRoutes = require('./routes');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(helmet());
-
 //configure rate limiter on login route
 const loginLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 5, // 5 requests,
 });
 
-app.use('/auth/login', loginLimiter);
-
 //configure rate limiter on signup route
 const signupLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, //1 hour
   max: 5, //5 requests
 });
-
-app.use('/auth/signup', signupLimiter);
 
 /* Authentication tools */
 // Express session configuration object
@@ -56,13 +48,62 @@ if (process.env.NODE_ENV === 'production') {
   sess.cookie.httpOnly = true;
 }
 
-app.use(session(sess));
-app.use(passport.initialize());
-app.use(passport.session());
-
-/* Mount routes */
-mountRoutes(app);
-
+// from heroku-cra-node
+const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  app.use(helmet());
+
+  //rate limiters
+  app.use('/auth/login', loginLimiter);
+  app.use('/auth/signup', signupLimiter);
+
+  app.use(session(sess));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  /* Mount routes */
+  mountRoutes(app);
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  // Answer API requests.
+  app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(
+      `Node ${
+        isDev ? 'dev server' : 'cluster worker ' + process.pid
+      }: listening on port ${PORT}`
+    );
+  });
+}
