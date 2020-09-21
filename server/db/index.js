a9f6c86b7e2850fcfa9211fdd5f245feb4d136b9
Cate <ceh2181@columbia.edu>
2020-09-20 23:52:34 -0400

    Prepare server db configuration for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -12,12 +12,15 @@
 require('dotenv').config();
 
 const { Pool } = require('pg');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+//connection string for local dev using env variables
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 });
 
 //both pool and query objects used elsewhere
