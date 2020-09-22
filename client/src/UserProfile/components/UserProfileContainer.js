f6957562ce2ca207dc69db3e4047b6b976734a7e
Cate <ceh2181@columbia.edu>
2020-09-21 21:55:21 -0400

    UserProfileContainer FIX cramped spacing on landscape mobile devices

--- client/src/UserProfile/components/UserProfileContainer.js
+++ client/src/UserProfile/components/UserProfileContainer.js
@@ -4,6 +4,13 @@ const UserProfileContainer = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
+
+  /* On landscape mobile devices */
+  @media (min-width: ${({ theme }) =>
+      theme.breakpoints.small}) and (max-width: ${({ theme }) =>
+      theme.breakpoints.medium}) {
+    padding-top: 20%;
+  }
 `;
 
 export default UserProfileContainer;
