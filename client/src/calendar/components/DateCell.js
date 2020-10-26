a094b0fdb066eef04c3e6d902070a9e95610aeb6
Cate <ceh2181@columbia.edu>
2020-10-26 16:28:36 -0400

    Make calendar date fonts bigger across screen sizes

--- client/src/calendar/components/DateCell.js
+++ client/src/calendar/components/DateCell.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 const DateCell = styled.td`
   color: ${({ theme }) => theme.colors.darkBrown60};
-  text-align: center;
+  font-size: 1.125rem;
   height: 3rem;
+  text-align: center;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.medium}) {
-    font-size: 1.3rem;
+    font-size: 1.375;
     height: 5.5rem;
   }
 `;
