79b497488fadb29c56937f73faa9c8a0ea28f60a
Cate <ceh2181@columbia.edu>
2020-10-26 16:30:24 -0400

    Make log doughnut bigger on mobile

--- client/src/log/components/LogDoughnut.js
+++ client/src/log/components/LogDoughnut.js
@@ -5,11 +5,11 @@ import styled from 'styled-components';
 import { withTheme } from 'styled-components';
 
 const LogDoughnutContainer = styled.div`
-  height: 44px;
+  height: 3.5rem;
   width: 100%;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.medium}) {
-    height: 64px;
+    height: 4rem;
   }
 `;
 
